023a8f25ee4aca022670593daf6eb77778983064
Oti Desmond <desmondotid.don16@gmail.com>
2024-04-08 12:28:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="container"> {/* Added a container for potential layout */}
+        <div className='widget'></div>
+      </div>
+  );
+}
+
+export default App;
+
